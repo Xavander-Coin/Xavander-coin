4a0a36fb646be2a17843ea734306856d8c9dd47c
TFinch <travisfinch01@gmail.com>
2019-08-05 21:16:20 -0400

    MN Collat Params fix

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2099,7 +2099,7 @@ bool CWallet::SelectCoinsDark(CAmount nValueMin, CAmount nValueMax, std::vector<
         if (out.tx->vout[out.i].nValue < CENT) continue;
         //do not allow collaterals to be selected
         if (IsCollateralAmount(out.tx->vout[out.i].nValue)) continue;
-		if (fMasterNode && wtx.vout[i].nValue == Params().MasternodeCollateralAmt()*COIN) continue;
+		if (fMasterNode && wtx.vout[i].nValue == Params().MasternodeCollateralAmt() * COIN) continue;
         //if (fMasterNode && out.tx->vout[out.i].nValue == 10000 * COIN) continue; //masternode input
 
         if (nValueRet + out.tx->vout[out.i].nValue <= nValueMax) {
